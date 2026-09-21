5d60982df8a25b208f1466a58294fbf2d2219877
Muravska <muravska@sheridancollege.ca>
2026-09-21 17:02:20 -0400

    Task 2 – Corner Bombs
    Instantiate a bomb at “inDistance” distance away from the
    player. The direction in which it will be spawned will be chosen randomly
    from one of the four corners around the player. Test with key 'C'

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -8,6 +8,8 @@ public class Player : MonoBehaviour
     public GameObject bombPrefab; 
     public Vector2 bombOffset;
     public List<Transform> asteroidTransforms; 
+
+    public float cornerBombDistance; // Distance from the player to spawn the bomb on a random corner
      
  
     void Start() 
@@ -24,6 +26,11 @@ public class Player : MonoBehaviour
         {
         SpawnBombAtOffset(bombOffset);
     }
+
+    if (Keyboard.current.cKey.wasPressedThisFrame)
+{
+    SpawnBombOnRandomCorner(cornerBombDistance);
+}
  
     } 
  
@@ -58,6 +65,32 @@ public class Player : MonoBehaviour
         SpawnBombAtOffset(offset);
     }
 }
+
+public void SpawnBombOnRandomCorner(float inDistance) 
+{
+    int randomCorner = Random.Range(0, 4); // Generate a random number between 0 and 3
+
+    Vector3 direction;
+
+   if (randomCorner == 0) // Top-right corner
+{
+    direction = (Vector3.up + Vector3.right).normalized;
+}
+else if (randomCorner == 1) // Top-left corner
+{
+    direction = (Vector3.up + Vector3.left).normalized;
+}
+else if (randomCorner == 2) // Bottom-right corner
+{
+    direction = (Vector3.down + Vector3.right).normalized;
+}
+else // Bottom-left corner
+{
+    direction = (Vector3.down + Vector3.left).normalized;
+}
+SpawnBombAtOffset(direction * inDistance); // Spawn a bomb at the calculated offset
+}
+
 
 
 
